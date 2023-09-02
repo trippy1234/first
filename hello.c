305e4d6788c431907659a7dcbf39d933b1c0aaaf
trippy1234 <143877750+trippy1234@users.noreply.github.com>
2023-09-02 16:49:56 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int a=10;
-int b = 20;
-sum = a+b;
-printf("The sum of two numbers is",sum);
+  int a,b,sum;
+  int a=10;
+  int b = 20;
+  sum = a+b;
+  printf("The sum of two numbers is",sum);
+  return 0;
 }
